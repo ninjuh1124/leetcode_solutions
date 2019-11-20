a62cf21b4e02b7ba1d48e1e57da84b66790769ec
ninjuh1124 <32426314+ninjuh1124@users.noreply.github.com>
2019-11-20 12:59:20 -0500

    Create CountPrimes.java

---
+++ CountPrimes.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countPrimes(int n) {
+
+        int count = 0;
+        boolean[] prime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            prime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (!prime[i]) continue;
+            for (int j = i * i; j < n; j += i) {
+                prime[j] = false;
+            }
+        }
+        for (int i = 2; i < n; i++) {
+            if (prime[i]) count++;
+        }
+
+        return count;
+    }
+}
